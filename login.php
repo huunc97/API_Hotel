2a2b386c16cbb69104b8ee33218387f6df7cd318
huunc97 <42510848+huunc97@users.noreply.github.com>
2019-06-05 10:48:34 +0700

    api_login

--- login.php
+++ login.php
@@ -1,4 +1,3 @@
-
 <?php
 $servername = "localhost";
 $username = "root";
@@ -9,16 +8,31 @@ $conn = new mysqli($servername, $username, $password);
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $user=$_POST["hihi"];
-    //$pass=$_POST["haha"];
-    $stmt = $conn->prepare("select * from nguoidung "); 
+    $user=$_POST["user"];
+    $pass=$_POST["pass"];
+
+    $stmt = $conn->prepare("select * from nguoidung where user='$user' and pass='$pass' "); 
     $stmt->execute();
     $kq=$stmt->fetchAll(PDO::FETCH_OBJ);
-    echo json_encode($kq);
-     if(count($kq)==1)
-     {
-      echo "{errorCode:0,message:'thanh cong'}";
-     }
+    //echo json_encode($kq);
+        if(count($kq)==1)
+        {
+            // foreach ($kq as $key => $value) {
+                $hoten=$kq[0]->hoten_nguoidung;
+                $gioitinh=$kq[0]->gioitinh_nguoidung;
+                $ngaysinh=$kq[0]->ngaysinh_nguoidung;
+                // $songaylam=$kq[0]->songaylam;
+                // $luongcoban=$kq[0]->luongcoban;
+                $chucvu=$kq[0]->chucvu;
+                //echo $kq[0]->hoten_nguoidung;
+                echo "{errorCode:0,hoten_nguoidung:'$hoten',gioitinh_nguoidung:'$gioitinh',ngaysinh_nguoidung:'$ngaysinh',chucvu:'$chucvu'}";
+             // }
+         
+
+            //echo "{errorCode:0,message:'Thành công'}";
+        }
+      //echo "{errorCode:0,message:'$user,$pass'}";
+     //}
      else
      {
          echo "{errorCode:1,message:'that bai'}";
